54b15ec591aeadc2a4e4ca063cec19d8c43babc7
lcallefe <luciana_donadio@hotmail.com>
2024-03-27 15:43:34 -0300

    fazendo teste passar

--- Ruby/unitarios/spec/saque_spec.rb
+++ Ruby/unitarios/spec/saque_spec.rb
@@ -1,14 +1,30 @@
+class ContaCorrente
+  attr_accessor :saldo
+
+  def initialize(saldo, valor = 0.0)
+    @saldo = saldo
+    @valor = valor
+  end
+
+
+  def saca
+    @saldo -= @valor
+  end
+end  
+
+
+
 describe ContaCorrente do
   describe 'Saque' do
     context 'quando o valor é positivo' do
       before(:all) do
-        conta = ContaCorrente.new(1000.00)
-        conta.saca(200)
+        @conta = ContaCorrente.new 1000.00, 200.00
+        @conta.saca
       end
 
       it 'então atualiza saldo' do
-        expect(conta.saldo).to eql 800.00
-
+        p @conta
+        expect(@conta.saldo).to eql 800.00
       end
     end 
   end
