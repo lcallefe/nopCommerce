4ad89cb406c3e02c06737be0ce94bb3745d21688
lcallefe <luciana_donadio@hotmail.com>
2024-03-27 23:19:40 -0300

    Finalizando cenários de login

--- Ruby/capy/spec/login_spec.rb
+++ Ruby/capy/spec/login_spec.rb
@@ -12,7 +12,25 @@ describe 'Meu primeiro script' do
 
     class_name = '.oxd-userdropdown-name'
     expect(find(class_name).visible?).to be true
-    expect(find(class_name).text).to eql 'Bureau Judiciaire du foncier'
+    expect(find(class_name)).to have_content 'Bob Tester'
+  end
+
+  it 'senha incorreta' do
+    visit "https://opensource-demo.orangehrmlive.com/"
+    fill_in 'username', with: 'Admin'
+    fill_in 'password', with: 'admin'
+    click_button 'Login'
+
+    expect(page).to have_content 'Invalid credentials'
+  end
+
+  it 'usuário não cadstrado' do
+    visit "https://opensource-demo.orangehrmlive.com/"
+    fill_in 'username', with: 'luciana'
+    fill_in 'password', with: 'admin123'
+    click_button 'Login'
+
+    expect(page).to have_content 'Invalid credentials'
   end
 end
 
